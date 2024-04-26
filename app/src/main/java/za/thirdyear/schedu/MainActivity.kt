f15f670b69db68468ef995b9d6a0ad361b373207
Mhlaba28 <ST10195811@vcconnect.edu.za>
2024-04-26 13:09:32 +0200

    Thr updated code. Login and registration working.

--- app/src/main/java/za/thirdyear/schedu/MainActivity.kt
+++ app/src/main/java/za/thirdyear/schedu/MainActivity.kt
@@ -1,64 +1,15 @@
 package za.thirdyear.schedu
 
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.widget.Toolbar
-import androidx.drawerlayout.widget.DrawerLayout
-import com.google.android.material.navigation.NavigationView
+
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navigationView: NavigationView
-    private lateinit var toolbar: Toolbar
-    override fun onCreate
-                (savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        drawerLayout = findViewById(R.id.drawer_layout)
-        navigationView = findViewById(R.id.navigationView)
-        toolbar = findViewById(R.id.toolbar)
-        val toggle = ActionBarDrawerToggle(
-            this,
-            drawerLayout,
-            toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_create_categories -> {
-                    val moveIntent = Intent(this, CreateCategoryActivity::class.java)
-                    startActivity(moveIntent)
-                    true
-                }
-                R.id.nav_view_categories -> {
-                    val moveIntent = Intent(this, ViewCategoriesActivity::class.java)
-                    startActivity(moveIntent)
-                    true
-                }
-                R.id.nav_display_details->{
-                    val moveIntent = Intent(this, DisplayDetails::class.java)
-                    startActivity(moveIntent)
-                    true
-                }
-                R.id.nav_logout->{
-                    val moveIntent = Intent(this, LoginActivity::class.java)
-                    startActivity(moveIntent)
-                    true
-                }
 
-
-
-                else -> false
-            }
-        }
     }
 }
-
-
